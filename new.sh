c9b38ab209a77babe9dd6ec8ff1988d01ae17982
2paperstar <lee@paperst.ar>
2023-08-23 17:27:22 +0900

    feat: undraft shell argument

--- new.sh
+++ new.sh
@@ -8,4 +8,18 @@ if [[ $2 == "draft" ]]; then
 else
   path="$Y/$M/$D"
 fi
+if [[ $2 == "undraft" ]]; then
+  directory="content/posts/drafts/$title"
+  if [[ -d $directory ]]; then
+    newDirectory="content/posts/$path/$title"
+    mv $directory $newDirectory
+    mv $newDirectory/index.md $newDirectory/index.md.bak
+  else
+    exit 1
+  fi
+fi
 hugo new content posts/$path/$title/index.md
+if [[ $2 == "undraft" ]]; then
+  cat $newDirectory/index.md.bak >> $newDirectory/index.md
+  rm $newDirectory/index.md.bak
+fi
